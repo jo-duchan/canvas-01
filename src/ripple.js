4a0aa1164cd3dd221cf5970368d6779699e3bcba
jo-duchan <jo_duchan@icloud.com>
2022-11-29 16:27:38 +0900

    중간 백업

---
+++ src/ripple.js
@@ -0,0 +1,25 @@
+export class Ripple {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.radius = 0;
+    this.maxRadius = 0;
+    this.speed = 10;
+  }
+
+  resize(stageWidth, stageHeight) {
+    this.stageWidth = stageWidth;
+    this.stageHeight = stageHeight;
+  }
+
+  start(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = 0;
+    this.maxRadius = this.getMax(x, y);
+  }
+
+  animate() {}
+
+  getMax(x, y) {}
+}
